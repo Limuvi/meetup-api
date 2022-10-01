8474e8dcd7a2e6c574019b6b0ac26f6cf4539c22
Victoria <ermak.victoria.vl@gmail.com>
2022-10-01 13:37:21 +0300

    Update meetup service

--- src/services/meetup.service.js
+++ src/services/meetup.service.js
@@ -1,46 +1,33 @@
-let meetups = [
-  {
-    id: 1,
-    title: 'Node.js meetup',
-    decription: 'blahblahblah',
-    tags: ['js', 'node.js', 'backend'],
-    date: '01.10.2022 12:30',
-    location: 'Vitebsk',
-  },
-  {
-    id: 2,
-    title: 'React.js meetup',
-    decription: 'blahblahblah',
-    tags: ['js', 'react.js', 'frontend'],
-    date: '01.10.2022 12:30',
-    location: 'Vitebsk',
-  },
-];
+const { Meetup } = require('../db');
 
-function find() {
+async function find() {
+  const meetups = await Meetup.findAll();
   return meetups;
 }
 
-function findById(id) {
-  const meetup = meetups.find((element) => element.id === +id);
+async function findById(id) {
+  const meetup = await Meetup.findByPk(id);
   return meetup;
 }
 
-function create(meetup) {
-  const id = meetups.length + 1;
-  meetups.push({ id, ...meetup });
+async function create(data) {
+  const meetup = Meetup.create(data);
 
-  return meetups;
+  return meetup;
 }
 
-function updateById(id, meetup) {
-  meetups[id - 1] = { id: +id, ...meetup };
-  return meetups;
+async function updateById(id, data) {
+  const [meetup] = await Meetup.update(data, {
+    where: { id },
+  });
+  return meetup;
 }
 
-function deleteById(id) {
-  meetups = meetups.filter((meetup) => meetup.id !== +id);
-  return meetups;
+async function deleteById(id) {
+  const deleted = await Meetup.destroy({
+    where: { id },
+  });
+  return deleted;
 }
 
 module.exports = {
