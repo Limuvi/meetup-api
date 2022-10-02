cc34965397dc15c12af07e9b622e24bc20ef0bbe
Victoria <ermak.victoria.vl@gmail.com>
2022-10-02 17:30:00 +0300

    Update meetup controller responses

--- src/controllers/meetup.controller.js
+++ src/controllers/meetup.controller.js
@@ -16,6 +16,11 @@ async function findMeetupById(req, res, next) {
   try {
     const { id } = req.params;
     const meetup = await meetupService.findById(id);
+
+    if (!meetup) {
+      throw new NotFoundError('Meetup is not found!');
+    }
+
     const dto = meetupMapper.mapMeetupToDto(meetup);
     return res.send(dto);
   } catch (error) {
@@ -40,8 +45,13 @@ async function updateMeetupById(req, res, next) {
     const { params: { id }, body } = req;
 
     const dto = meetupMapper.mapDtoToMeetup(body);
-    await meetupService.updateById(id, dto);
-    res.status(204).send();
+    const meetup = await meetupService.updateById(id, dto);
+
+    if (!meetup) {
+      const { id: newId } = await meetupService.create(dto);
+      return res.status(201).location(`/meetups/${newId}`).send();
+    }
+
     return res.status(204).send();
   } catch (error) {
     return next(error);
@@ -52,8 +62,12 @@ async function deleteMeetupById(req, res, next) {
   try {
     const { id } = req.params;
 
-    await meetupService.deleteById(id);
-    res.status(204).send();
+    const deleted = await meetupService.deleteById(id);
+
+    if (!deleted) {
+      throw new NotFoundError('Meetup is not found!');
+    }
+
     return res.status(204).send();
   } catch (error) {
     return next(error);
