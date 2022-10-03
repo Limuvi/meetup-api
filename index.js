9e480685b52a33384b6c4302ee1064961646089e
Victoria <ermak.victoria.vl@gmail.com>
2022-10-03 18:34:59 +0300

    Create swagger documentation

--- index.js
+++ index.js
@@ -1,16 +1,19 @@
 /* eslint-disable no-console */
+const fs = require('fs');
+const swaggerUi = require('swagger-ui-express');
 const express = require('express');
 const { initDB } = require('./src/db');
 const router = require('./src/routes');
 const { errorHandler } = require('./src/middlewares');
-require('dotenv').config();
 
-const { PORT } = process.env;
+const PORT = 7000;
 const app = express();
+const swaggerFile = JSON.parse(fs.readFileSync('./openapi.json'));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(router);
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerFile));
 app.use(errorHandler);
 
 app.listen(PORT, () => {
