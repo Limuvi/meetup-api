08be1d37cfec9c07d09af5d6fd1b06c2366a83ee
Victoria <ermak.victoria.vl@gmail.com>
2022-10-05 22:12:22 +0300

    Upsert organizer user on application startup

--- src/db/db.init.js
+++ src/db/db.init.js
@@ -1,13 +1,14 @@
 /* eslint-disable no-console */
 const db = require('./db.config');
 const { ROLE_ORGANIZER, ROLE_USER } = require('../constants');
+const { hashPassword } = require('../helpers');
 
 async function initDB() {
   try {
     await db.sequelize.sync();
     console.log('Connected to DB.');
 
-    await db.Role.findOrCreate({
+    const [organizerRole] = await db.Role.findOrCreate({
       where: { name: ROLE_ORGANIZER },
       defaults: {
         name: ROLE_ORGANIZER,
@@ -19,6 +20,26 @@ async function initDB() {
         name: ROLE_USER,
       },
     });
+
+    const username = 'admin';
+    const password = '12345';
+    const userData = {
+      username,
+      hashedPassword: hashPassword(password),
+      roleId: organizerRole.id,
+    };
+
+    const user = await db.User.findOne({
+      where: { username },
+    });
+
+    if (!user) {
+      await db.User.create(userData);
+    } else {
+      await user.update(userData);
+    }
+
+    console.log(`Upserted ${ROLE_ORGANIZER}`, { username, password });
   } catch (error) {
     console.log(`Failed to connect to DB: ${error.message}`);
   }
